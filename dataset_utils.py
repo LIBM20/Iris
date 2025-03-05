862a93d29dfb91c6eb4c194de76d64e6ccae12f6
LIBM20 <libm20@outlook.com>
2025-03-05 17:55:34 +0000

    Plot variables histograms

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pandas import DataFrame
+import matplotlib.pyplot as plt
 
 #Remover linhas duplicadas
 def remove_duplicated_lines(dataset):
@@ -11,3 +12,23 @@ def remove_duplicated_lines(dataset):
         print(f'Removed {removed} duplicated samples (rows)')
     print(f'Found {rows} samples (rows)')
     return dataset
+
+
+def plot_histogram(dataset, variable_name, color='blue', title=None, xlabel=None, ylabel=None):
+    data = dataset[variable_name]  # Select only the column by its name
+
+    if title is None:
+        title = f'{variable_name} histogram'
+    if xlabel is None:
+        xlabel = variable_name
+    if ylabel is None:
+        ylabel = 'Frequency'
+
+    plt.figure(figsize=(8, 6))
+    plt.hist(data, color=color, edgecolor='black', alpha=0.7)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.grid(True)
+    plt.show()
+
--- pre_processing.py
+++ pre_processing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from dataset_utils import remove_duplicated_lines
+from dataset_utils import remove_duplicated_lines, plot_histogram
 
 dataset = pd.read_csv("./data/iris.data", header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
 print(dataset)
@@ -8,6 +8,8 @@ print(dataset)
 dataset = remove_duplicated_lines(dataset)
 
 dataset.info()
+
+plot_histogram(dataset, 'sepal_length')
 
 print(dataset.describe(include='all'))
 
@@ -17,3 +19,6 @@ if not os.path.exists('./pre_processed'):
     os.mkdir('./pre_processed')
 
 dataset.to_csv('./pre_processed/iris.csv')
+
+
+
