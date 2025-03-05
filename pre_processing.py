67bc4f037bfef99543563967bef3ffa057170ca9
LIBM20 <libm20@outlook.com>
2025-03-05 17:21:13 +0000

    Save pre-processed dataset

--- pre_processing.py
+++ pre_processing.py
@@ -5,3 +5,10 @@ dataset = pd.read_csv("./data/iris.data", header=None)
 print(dataset)
 
 dataset = remove_duplicated_lines(dataset)
+
+import os
+#Se a pasta não existir é criada
+if not os.path.exists('./pre_processed'):
+    os.mkdir('./pre_processed')
+
+dataset.to_csv('./pre_processed/iris.csv')
